424b66ced52ad26f4db9825a9f4a2323ab6afa5f
Ayush Kumar <ayushkumar1708@gmail.com>
2023-08-26 23:47:03 +0530

    provide dyanamic character to the header and design the onload-spinner

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,43 +1,81 @@
-import React from 'react'
-import { useLocation, useNavigate } from 'react-router-dom'
-
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import React, { useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 
 export default function Header() {
-  const location = useLocation()
-  const navigate = useNavigate()
-  function pathMatchRoute(route){
-    if(route === location.pathname){
-      return true
+  const location = useLocation();
+  const navigate = useNavigate();
+  const [pageState, setPageState] = useState("Sign In")
+  const auth = getAuth()
+  useEffect(()=>{
+    onAuthStateChanged(auth, (user)=>{
+      if(user){
+        setPageState("Profile")
+      }else{
+        setPageState("Sign In")
+      }
+    })
+  })
+  function pathMatchRoute(route) {
+    if (route === location.pathname) {
+      return true;
     }
   }
   return (
-    <div className='bg-white border-b shadow-md sticky top-0 z-50'>
-        <header className='flex justify-between items-center
-        px-3 max-w-6xl mx-auto'>
-          <div>
-            <img src="https://images.squarespace-cdn.com/content/v1/5eaae9148bac171763985224/1588935233944-DV8B2NRZVHLC3AXN4D6R/booknow.png" alt="logo" className='h-14 cursor-pointer' onClick={()=>navigate('/')} />
-          </div>
-          <div>
-            <ul className="flex space-x-10">
-              <li className={`cursor-pointer py-3 text-sm font-semibold
+    <div className="bg-white border-b shadow-md sticky top-0 z-40">
+      <header
+        className="flex justify-between items-center
+        px-3 max-w-6xl mx-auto"
+      >
+        <div>
+          <img
+            src="https://images.squarespace-cdn.com/content/v1/5eaae9148bac171763985224/1588935233944-DV8B2NRZVHLC3AXN4D6R/booknow.png"
+            alt="logo"
+            className="h-14 cursor-pointer"
+            onClick={() => navigate("/")}
+          />
+        </div>
+        <div>
+          <ul className="flex space-x-10">
+            <li
+              className={`cursor-pointer py-3 text-sm font-semibold
               text-gray-400 border-b-[3px] border-b-transparent
-              ${pathMatchRoute("/") && " text-black font-bold border-b-red-500"}`}
-              onClick={()=>navigate('/')}>Home</li>
+              ${
+                pathMatchRoute("/") && " text-black font-bold border-b-red-500"
+              }`}
+              onClick={() => navigate("/")}
+            >
+              Home
+            </li>
 
-              <li className={`${pathMatchRoute("/offers") && "font-bold text-black border-b-red-500"}
+            <li
+              className={`
               cursor-pointer py-3 text-sm font-semibold
               text-gray-400 border-b-[3px] border-b-transparent
+              ${
+                pathMatchRoute("/offers") &&
+                "font-bold text-black border-b-red-500"
+              }
               `}
-              onClick={()=>navigate('/offers')}>Offers</li>
+              onClick={() => navigate("/offers")}
+            >
+              Offers
+            </li>
 
-              <li className={`cursor-pointer py-3 text-sm font-semibold
+            <li
+              className={`cursor-pointer py-3 text-sm font-semibold
               text-gray-400 border-b-[3px] border-b-transparent
-              ${pathMatchRoute("/sign-in") && " border-b-red-500"}`}
-              onClick={()=>navigate('/sign-in')}>Sign In</li>
-            </ul>
-          </div>
-        </header>
+              ${
+                (pathMatchRoute("/sign-in") || pathMatchRoute("/profile")) &&
+                "text-black font-bold border-b-red-500"
+              }`}
+              onClick={() => navigate("/profile")}
+            >
+             {pageState}
+            </li>
+          </ul>
+        </div>
+      </header>
     </div>
-  )
+  );
 }
-
--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -1,10 +1,14 @@
 import { useState } from "react"
-import { getAuth} from "firebase/auth";
+import { getAuth, updateProfile} from "firebase/auth";
 import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import { doc, updateDoc } from "firebase/firestore";
+import {db} from "../firebase"
 
 export default function Profile() {
   const auth = getAuth()
   const navigate = useNavigate()
+  const [editDetail, setEditDetail]= useState(false)
   const [formData, setFormData] = useState({
     name: auth.currentUser.displayName,
     email:auth.currentUser.email,
@@ -14,6 +18,28 @@ export default function Profile() {
     auth.signOut()
     navigate("/")
   }
+  function onChange(e){
+    setFormData((prevState)=>({
+      ...prevState,
+      [e.target.id] : e.target.value
+    }))
+  }
+  async function onSubmit(){
+    try {
+      if(auth.currentUser.displayName !== name){
+        await updateProfile(auth.currentUser,{
+          displayName:name,
+        })
+        const docReference= doc(db, "users", auth.currentUser.uid)
+        await updateDoc(docReference,{
+          name,
+        })
+      }
+      toast.success('Profile details updated!')
+    } catch (error) {
+      toast.error("Could not make the changes")
+    }
+  }
   return (
     <>
       <section className="max-w-6xl mx-auto flex justify-center items-center flex-col ">
@@ -21,11 +47,14 @@ export default function Profile() {
         <div className="w-full md:w-[50%] mt-6 px-3 ">
           <form >
             {}
-            <input type="text" id='name' value={name} disabled className="w-full px-4 py-2 mb-6  text-xl text-gray-700 bg-white border border-gray-300 rounded transition ease-in-out"/>
+            <input type="text" id='name' value={name} disabled={!editDetail} onChange={onChange} className="w-full px-4 py-2 mb-6  text-xl text-gray-700 bg-white border border-gray-300 rounded transition ease-in-out"/>
             {}
-            <input type="email" id='email' value={emailmb-6 } disabled className="w-full px-4 py-2 mb-6 text-xl text-gray-700 bg-white border border-gray-300 rounded transition ease-in-out"/>
+            <input type="email" id='email' value={email} disabled className={`w-full px-4 py-2 mb-6 text-xl text-gray-700 bg-white border border-gray-300 rounded transition ease-in-out ${editDetail && "bg-red-200 focus:bg-red-200 " }`}/>
             <div className="flex justify-between whitespace-nowrap text-sm sm:text-lg mb-6">
-              <p className="flex items-center ">Do you wish to change you name?<span className="text-red-600 hover:text-red-700 transition ease-in-out duration-200 ml-1 cursor-pointer">Edit</span></p>
+              <p className="flex items-center ">Do you wish to change you name?
+              <span onClick={()=>{editDetail && onSubmit();  setEditDetail((prevState)=>!prevState)}} className="text-red-600 hover:text-red-700 transition ease-in-out duration-200 ml-1 cursor-pointer">
+                {editDetail ? "Apply changes" : "Edit"}
+              </span></p>
               <p onClick={onLogout} className="text-blue-600 hover:text-blue-700 transition ease-in-out duration-200 cursor-pointer">Sign out</p>
             </div>
           </form>
